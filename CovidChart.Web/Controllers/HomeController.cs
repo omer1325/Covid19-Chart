5dd204c118651dd478e1ce876ef72eadd506bbd5
Ömer Faruk İngeç <omer.ingec24@gmail.com>
2022-01-12 23:09:53 +0300

    Completed
    I developed a small project using .NET 5.0, Angular and SignalR technologies. My aim is to show real-time Covid19 case count using SignalR library. I developed two different projects to measure performance in the front-end part. You can see the Angular project on the left side of the screen and the .NET MVC project on the right. I coded the back-end part with .NET 5.0. When I send a request to the service, it randomly writes Covid19 cases for 5 cities into the database. As we write the data to the database, we can instantly see the number of cases in our graphic table.

---
+++ CovidChart.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CovidChart.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidChart.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Covid19Show()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
